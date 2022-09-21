71336ff4c13f5d9efbe41147d7c96d9f09870c39
camelcrush <dkxm0927@naver.com>
2022-09-21 22:52:30 +0900

    Unit Testing 2

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -43,4 +43,39 @@ describe('MoviesService', () => {
       }
     });
   });
+
+  describe('deleteOne', () => {
+    it('deletes a movie', () => {
+      service.create({
+        title: 'test',
+        year: 2022,
+        genres: ['test'],
+      });
+      const beforeDelete = service.getAll().length;
+      service.deleteOne(1);
+      const afterDelete = service.getAll().length;
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    });
+
+    it('should throw 404 error', () => {
+      try {
+        service.deleteOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('create', () => {
+    it('creates a movie', () => {
+      const beforeCreate = service.getAll().length;
+      service.create({
+        title: 'test',
+        year: 2022,
+        genres: ['test'],
+      });
+      const afterCreate = service.getAll().length;
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
 });
